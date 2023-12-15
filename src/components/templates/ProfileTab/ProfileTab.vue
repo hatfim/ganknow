70f97c723f6450e90dc1c86f43e3485100f70b71
Hatfim Ydniw <hatfim.ydniw@gmail.com>
2023-12-16 00:26:02 +0700

    feat(component): 🚀 update view

--- src/components/templates/ProfileTab/ProfileTab.vue
+++ src/components/templates/ProfileTab/ProfileTab.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid md:grid-cols-5 gap-4">
+  <div class="grid grid-cols-1 md:grid-cols-5 gap-4">
     <div class="col-span-2">
       <div class="flex flex-col bg-card p-4 md:rounded-lg drop-shadow">
         <h3 class="text-sm md:text-xl font-bold text-white mb-4 whitespace-pre-line">
--- src/pages/PostPage/PostPage.vue
+++ src/pages/PostPage/PostPage.vue
@@ -1,15 +1,32 @@
 <template>
-  <main class="grid grid-cols-1 md:grid-cols-5 max-w-6xl mx-auto px-4 lg:px-8">
-    <section class="col-span-3">
+  <main class="grid grid-cols-1 md:grid-cols-5 gap-4 max-w-6xl mx-auto px-4 lg:px-8 mt-8">
+    <section class="col-span-3 order-2 md:order-1">
       <v-feed-card v-if="feed" :feed="feed" />
     </section>
+    <div class="col-span-2 order-1 md:order-2">
+      <div class="flex flex-col justify-center items-center gap-4 bg-card p-4 md:rounded-lg drop-shadow">
+        <v-avatar :img="userProfile.avatar" />
+        <h2 class="inline-flex items-center space-x-2">
+          <span class="text-2xl font-bold text-primary-foreground">{{
+            userProfile.nickname
+          }}</span>
+          <check-badge-icon class="h-5 w-5 text-primary" />
+        </h2>
+        <h3 class="text-sm md:text-base text-white mb-4 text-center">
+          {{ userProfile.description }}
+        </h3>
+      </div>
+    </div>
   </main>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useStore } from 'vuex'
 
+import { CheckBadgeIcon } from '@heroicons/vue/24/solid'
+
+import VAvatar from '@/components/atoms/VAvatar'
 import VFeedCard from '@/components/organisms/VFeedCard'
 
 const feed = ref(null)
@@ -22,11 +39,15 @@ const props = defineProps({
   },
 })
 
+const userProfile = computed(() => store.getters['profile/getProfile'])
+
 onMounted(async () => {
   try {
     const fetchedFeed = await store.dispatch('feeds/fetchFeed', props.postId)
+
     if (fetchedFeed) {
       feed.value = fetchedFeed
+      await store.dispatch('profile/fetchUserProfile', fetchedFeed.authorUser.nickname)
     } else {
       console.error('No feed data returned for postId:', props.postId)
     }
@@ -34,4 +55,5 @@ onMounted(async () => {
     console.error('Failed to fetch feed:', error)
   }
 })
+
 </script>
